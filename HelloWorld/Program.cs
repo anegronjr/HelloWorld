69069c0787615a83836162e0f9d34e1f9a4130ac
Artie Negron <anegroncsu@gmail.com>
2019-01-15 15:19:31 -0500

    Added early guessing game. Need to add multiple attempts if guess is not correct.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,31 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int winningNum;
+            int inputNum;
+            string input;
+
+            winningNum = 7;
+
+            Console.WriteLine("Guess the winning number between 1-10:");
+            input = Console.ReadLine();
+
+            bool isNumber = int.TryParse(input, out inputNum);
+
+            if (isNumber == false)
+            {
+                Console.WriteLine("You must enter a number.");
+            } else if (inputNum <= 0 || inputNum >= 11)
+            {
+                Console.WriteLine("You must enter a number between 1 and 10.");
+            } else if (inputNum == winningNum)
+            {
+                Console.WriteLine($"You've won! The winning number is {winningNum}.");
+            } else
+            {
+                Console.WriteLine("Sorry, the winning number was {winningNum}.");
+            }
+
         }
     }
 }
